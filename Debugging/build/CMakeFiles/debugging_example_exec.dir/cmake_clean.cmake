66b2214958a9295fedbcceb97161527a81858d71
AngusBach <156666508+AngusBach@users.noreply.github.com>
2024-02-26 11:56:18 +0100

    lol

---
+++ Debugging/build/CMakeFiles/debugging_example_exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debugging_example_exec.dir/debugging.c.o"
+  "CMakeFiles/debugging_example_exec.dir/debugging.c.o.d"
+  "debugging_example_exec"
+  "debugging_example_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/debugging_example_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Debugging/build/CMakeFiles/debugging_example_exec.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for debugging_example_exec.
+# This may be replaced when dependencies are built.
